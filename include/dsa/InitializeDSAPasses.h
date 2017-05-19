a4b21d3b75b725541077898da2877a496b734b47
Yishen Chen <cyt046@gmail.com>
2017-05-18 23:39:14 -0500

    first code drop

---
+++ include/dsa/InitializeDSAPasses.h
@@ -0,0 +1,57 @@
+#ifndef LLVM_INITIALIZE_DSA_PASSES
+#define LLVM_INITIALIZE_DSA_PASSES
+
+namespace llvm {
+
+class PassRegistry;
+
+// AllocIdentify
+void initializeAllocIdentifyPass(PassRegistry &);
+
+// AddressTakenAnalysis
+void initializeAddressTakenAnalysisPass(PassRegistry &);
+
+// BasicDataStructures
+void initializeBasicDataStructuresPass(PassRegistry &);
+
+// BUDataStructures
+void initializeBUDataStructuresPass(PassRegistry &);
+
+// CompleteBUDataStructures
+void initializeCompleteBUDataStructuresPass(PassRegistry &);
+
+// DSGraphStats
+void initializeDSGraphStatsPass(PassRegistry &);
+
+// EntryPointAnalysis
+void initializeEntryPointAnalysisPass(PassRegistry &);
+
+// EquivBUDataStructures
+void initializeEquivBUDataStructuresPass(PassRegistry &);
+
+// DSGC
+void initializeDSGCPass(PassRegistry &);
+
+// LocalDataStructures
+void initializeLocalDataStructuresPass(PassRegistry &);
+
+// SanityCheck
+void initializeSanityCheckPass(PassRegistry &);
+
+// StdLibDataStructures
+void initializeStdLibDataStructuresPass(PassRegistry &);
+
+// TDDataStructures
+void initializeTDDataStructuresPass(PassRegistry &);
+
+// EQTDDataStructures
+void initializeEQTDDataStructuresPass(PassRegistry &);
+
+// TypeSafety<EQTDDataStructures>
+void initializeTypeSafetyEQTD(PassRegistry &);
+
+// TypeSafety<TDDataStructures>
+void initializeTypeSafetyTD(PassRegistry &);
+}
+
+#endif
